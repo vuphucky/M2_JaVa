f64b01075f748a61b7398cee229d2cede239480e
vuphucky <phucky1235@gmail.com>
2024-07-07 02:25:37 +0700

    thuc hanh date

---
+++ src/intro_to_java/date_time.java
@@ -0,0 +1,10 @@
+package intro_to_java;
+
+import java.util.Date;
+
+public class date_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.print("Now is: " + now);
+    }
+}
